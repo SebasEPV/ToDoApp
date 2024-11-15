33009060e967a8deecf56b9029accd61b2b6e808
SebasEPV <sperezvinas@gmail.com>
2024-11-15 07:37:08 -0500

    Task Module

--- backend/src/task/task.controller.ts
+++ backend/src/task/task.controller.ts
@@ -1,34 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { TaskService } from './task.service';
-import { Task } from '@prisma/client';
-import { identity } from 'rxjs';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('task')
 export class TaskController {
-    constructor(private taskService : TaskService) {};
+  constructor(private readonly taskService: TaskService) {}
 
-    @Get()
-    getAllTasks(){
-        return this.taskService.getAllTasks()
-    }
+  @Post()
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.taskService.create(createTaskDto);
+  }
 
-    @Get(':id')
-    getTaskById(@Param('id') id : number){
-        return this.taskService.getTaskById(id)
-    }
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
 
-    @Post(':id')
-    createTask(@Body() data : Task){
-        return this.taskService.createTask(data)
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.taskService.findOne(+id);
+  }
 
-    @Delete(':id')
-    deleteTask(@Param('id') id : number) {
-        return this.taskService.deleteTask(id)
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.taskService.update(+id, updateTaskDto);
+  }
 
-    @Patch(':id/:status')
-    updateTaskStatus(@Param('id') id : number, @Param('status') status : number) {
-        return this.taskService.updateStatus(id, status)
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.taskService.remove(+id);
+  }
 }
