c89edd5b58bc81b245fbafbd6b0c01f5d0d81507
SebasEPV <sperezvinas@gmail.com>
2024-11-22 07:46:13 -0500

    Added validations and Create method

--- backend/src/activity/activity.service.ts
+++ backend/src/activity/activity.service.ts
@@ -1,26 +1,35 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateActivityDto } from './dto/create-activity.dto';
 import { UpdateActivityDto } from './dto/update-activity.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ActivityService {
-  constructor (private prisma : PrismaService) {}
-  create(createActivityDto: CreateActivityDto) {
-    return 'This action adds a new activity';
+  constructor(private prisma: PrismaService) {}
+  create(activity: CreateActivityDto) {
+    return this.prisma.activity.create({data : activity});
   }
 
   async findAll() {
-    return this.prisma.activity.findMany();
+    const activities = this.prisma.activity.findMany();
+    if (!activities) {
+      throw new NotFoundException('Actividades no encontradas');
+    }
   }
 
   async findOne(id: number) {
+    if (!id) {
+      throw new BadRequestException('El ID es obligatorio.');
+    }
     const activity = this.prisma.activity.findUnique({
       where: { id },
     });
 
-
-    return activity;  }
+    if (!activity) {
+      throw new NotFoundException('Actividad no encontrada');
+    }
+    return activity;
+  }
 
   update(id: number, updateActivityDto: UpdateActivityDto) {
     return `This action updates a #${id} activity`;
--- backend/src/activity/dto/create-activity.dto.ts
+++ backend/src/activity/dto/create-activity.dto.ts
@@ -1,1 +1,18 @@
-export class CreateActivityDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateActivityDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  startTime: string;
+
+  @IsString()
+  endTime: string;
+
+  @IsNumber()
+  taskId: number;
+}
