d07e63723db1b66d678b98dd3936906ac667ea6c
SebasEPV <sperezvinas@gmail.com>
2024-11-22 07:46:20 -0500

    Added validations and Create method

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTaskDto {}
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+
+export enum Priority {
+  TOP = 'TOP',
+  DUMP = 'DUMP',
+}
+
+export class CreateTaskDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsEnum(Priority)
+  priority: Priority;
+
+  @IsString()
+  dueDate: string;
+
+  @IsNumber()
+  userId: number;
+}
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,43 +1,47 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TaskService {
-  constructor (private prisma : PrismaService){}
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(private prisma: PrismaService) {}
+  create(task: CreateTaskDto) {
+    return this.prisma.task.create({data : task});
   }
 
   findAll() {
-    const tasks = this.prisma.task.findMany() 
-    if (tasks !== null) {
-      return tasks;
+    const tasks = this.prisma.task.findMany();
+    if (!tasks) {
+      throw new NotFoundException('Tareas no encontradas');
     }
-    return "No hay usuarios registrados"
+    return tasks;
   }
 
-  findOne(id: number) {
-    const task = this.prisma.task.findUnique({
-      where : {id}
+  async findOne(id: number) {
+    if (!id) {
+      throw new BadRequestException('El ID es obligatorio.');
+    }
+    const task = await this.prisma.task.findUnique({
+      where: { id },
     });
-
-
+  
+    if (!task) {
+      throw new NotFoundException('Tarea no encontrada');
+    }
     return task;
-
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
     return this.prisma.task.update({
-      where : {id},
-      data : updateTaskDto
+      where: { id },
+      data: updateTaskDto,
     });
   }
 
   remove(id: number) {
     return this.prisma.task.delete({
-      where : {id}
+      where: { id },
     });
   }
 }
